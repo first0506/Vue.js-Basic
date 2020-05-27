391f127eaa6bfdfdaf7f41671a31bef7fd7b88d6
김현준 <first0506@naver.com>
2020-05-27 15:30:46 +0900

    vue-router-tutorial

---
+++ 03_first-vue-cli/src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+        <h1>Contact Me!</h1>
+        <p>연락주세요</p>
+  </div>
+</template>
+
+<script>
+// views/ 있는 
+export default {
+    name: 'Contact',
+}
+</script>
+
+<style>
+
+</style>
